2c8b877473f9a24f483c356dc828d249b5041880
JOANA CASTELLÓ VISIEDO <joacasvi@upv.edu.es>
2022-09-08 14:00:00 +0200

    Basic IU finished. Bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchBarComponent,
+    MovieListComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
